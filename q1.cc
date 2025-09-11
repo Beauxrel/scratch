4410bb8420d6b350e62530b318138a1dfd997b50
Beauxrel <landonbjackson@gmail.com>
2025-09-10 20:44:29 -0400

    - Enabled PCAP tracing

--- q1.cc
+++ q1.cc
@@ -62,6 +62,7 @@ main(int argc, char* argv[])
     clientApps.Start(Seconds(2));
     clientApps.Stop(Seconds(10));
 
+    pointToPoint.EnablePcapAll("q1");
     Simulator::Run();
     Simulator::Destroy();
     return 0;
